3d95e7f3d2d271c8b0fa5123a55eb1dd5eef479a
Nectarios UofA-TUC-FSF-PT-02-2020-U-C <npgockel@gmail.com>
2020-02-29 19:19:07 -0700

    work checkpoint

--- pseudo.md
+++ pseudo.md
@@ -1,29 +1,30 @@
 ### Criteria
+
 **1.** Button to generate password
 
-a. **Clicking button** activates showing **user choices** for password criteria.  Choices include:
+a. **Clicking button** activates showing **user choices** for password criteria. Choices include:
 
- a. i. Password length from 8-128
+a. i. Password length from 8-128
 
- a. ii. Character types: "ABCDEFGHIJKLMNOPQRSTUVWZYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
+a. ii. Character types: "ABCDEFGHIJKLMNOPQRSTUVWZYZabcdefghijklmnopqrstuvwxyz1234567890!@#\$%^&*()+";
 
- a. ii. 1. Uppercase "ABCDEFGHIJKLMNOPQRSTUVWZYZ";
+a. ii. 1. Uppercase "ABCDEFGHIJKLMNOPQRSTUVWZYZ";
 
-*and/or*
+_and/or_
 
- a. ii. 2. Lowercase "abcdefghijklmnopqrstuvwxyz";
+a. ii. 2. Lowercase "abcdefghijklmnopqrstuvwxyz";
 
-*and/or*
+_and/or_
 
- a. ii. 3. Numeral "1234567890";
+a. ii. 3. Numeral "1234567890";
 
-*and/or*
+_and/or_
 
- a. ii. 4. Special Characters "!@#$%^&*()_+";
+a. ii. 4. Special Characters "!@#\$%^&*()+";
 
 **2.** Combine **at least one character** from input selection(s) (1.a.ii.1-4)
 
-**3.**  **Validate** that the generated password is
+**3.** **Validate** that the generated password is
 a. The selected **length**
 b. **Contains** at least one of each selected character type
 
--- script.js
+++ script.js
@@ -1,14 +1,83 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+// // Assignment Code
+// var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+//   passwordText.value = password;
 
+// }
+
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
+
+// HTML idss
+
+// Function to generate password
+function generatePassword() {
+  // Password length
+  var passLength = document.getElementById("length").value;
+
+  // Character Choice Classes (CCC)
+  var upperChar = "ABCDEFGHIJKLMNOPQRSTUVWZYZ";
+  var lowerChar = "abcdefghijklmnopqrstuvwxyz";
+  var numerChar = "1234567890";
+  var speciChar = "!@#$%^&*()_+";
+
+  // Containers for CCC (Boolean)
+  var addUpper = false;
+  var addLower = false;
+  var addNumer = false;
+  var addSpeci = false;
+
+  // Event listeners for CCC
+
+  document.addEventListener("DOMContentLoaded", function(event) {
+    var checkUpper = document.querySelector("input[name=upper-char]");
+    checkUpper.addEventListener("change", function(event) {
+      if (checkUpper.checked) {
+        addUpper = true;
+        console.log(addUpper)
+      } else {
+        // do something else otherwise
+      }
+    });
+  });
+
+  var checkLower = document.querySelector("input[name=lower-char]");
+
+  checkLower.addEventListener("change", function() {
+    if (this.checked) {
+      // Checkbox is checked..
+    } else {
+      // Checkbox is not checked..
+    }
+  });
+
+  var checkNumer = document.querySelector("input[name=numer-char]");
+
+  checkNumer.addEventListener("change", function() {
+    if (this.checked) {
+      // Checkbox is checked..
+    } else {
+      // Checkbox is not checked..
+    }
+  });
+
+  var checkSpeci = document.querySelector("input[name=speci-char]");
+
+  checkSpeci.addEventListener("change", function() {
+    if (this.checked) {
+      // Checkbox is checked..
+    } else {
+      // Checkbox is not checked..
+    }
+  });
+
+  // The generated password container
+  var password = "";
+
+  document.getElementById("password").value = password;
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
